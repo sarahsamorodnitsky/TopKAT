9311fd82d024af4df51d1bf68eb919ed990aa999
Sarah Samorodnitsky <35846144+sarahsamorodnitsky@users.noreply.github.com>
2024-11-15 19:19:16 -0600

    Add nperm

--- R/globals.R
+++ R/globals.R
@@ -1,3 +1,3 @@
 utils::globalVariables(c("PID", "Var1", "Var2", "birth", "death", "dimension",
                          "from", "from.x", "from.y", "group", "to", "to.x",
-                         "to.y", "total.count", "value", "x", "y"))
+                         "to.y", "total.count", "value", "x", "y", "nperm"))
